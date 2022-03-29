daa7c1ff95949b66982e5424b8ceb5bf1e122338
saprabh20 <55506636+saprabh20@users.noreply.github.com>
2022-03-29 12:21:11 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,45 @@
+import { update as updateSnake, draw as drawSnake, Snake_Speed, getSnakeHead, snakeIntersection } from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js"
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById("game-board");
+
+function main(currentTime) {
+    if(gameOver){
+        if(confirm("You Lost! Press Ok to restart.")){
+            window.location= "/DEV/Jquery/Snake";
+        }
+        return;
+    }
+    
+    window.requestAnimationFrame(main);
+
+    const secsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secsSinceLastRender < 1 / Snake_Speed) {
+        return;
+    }
+
+    lastRenderTime = currentTime;
+
+    updateS()
+    drawS()
+}
+window.requestAnimationFrame(main);
+
+function updateS() {
+    updateSnake();
+    updateFood();
+    checkDeath();
+}
+
+function drawS() {
+    gameBoard.innerHTML = "";
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
